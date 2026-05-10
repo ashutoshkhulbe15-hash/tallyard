0d34b82f3d4eb6212519e1a4cc2106a938aa554a
ashutoshkhulbe15-hash <ashutoshkhulbe15@gmail.com>
2026-05-11 03:09:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,13 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-7LB5KS70CD');`}
         </Script>
+        <Script
+  id="ms-clarity"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "wp47rtb8ix");`,
+  }}
+/>
       </head>
       <body>
         <UnitProvider>
